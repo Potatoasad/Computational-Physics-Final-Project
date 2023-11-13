51134ea43055d4f15ea5a278c91a105fc545609a
Asad Hussain <asadhussain625@gmail.com>
2023-11-13 12:26:11 -0600

    added bayesian_inference package

---
+++ bayesian_inference/distribution.py
@@ -0,0 +1,14 @@
+class Distribution:
+	"""
+    A class to represent a distribution
+    ...
+
+    Attributes
+    ----------
+    parameters : dict
+        A dictionary containing the pdf parameters of this distribution
+
+    Methods
+    -------
+    pdf(x) : 
+    """
