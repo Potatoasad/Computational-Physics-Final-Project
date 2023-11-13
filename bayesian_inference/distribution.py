7295acc2b3e1e5ba7911ceffdf89c82ad498e1ae
Asad Hussain <asadhussain625@gmail.com>
2023-11-13 12:30:50 -0600

    added distribution documentation

--- bayesian_inference/distribution.py
+++ bayesian_inference/distribution.py
@@ -5,10 +5,18 @@ class Distribution:
 
     Attributes
     ----------
-    parameters : dict
-        A dictionary containing the pdf parameters of this distribution
+    name: str
+    	stores the name of the distribution
 
     Methods
     -------
-    pdf(x) : 
+    pdf(x) : vector -> float
+    	returns the probability density function (PDF) at the point x
+
+    logpdf(x) vector -> float: 
+    	returns the log of the probability density function (PDF) at point x
+
+    dlogpdf(x) jax array -> float:
+    	returns the log of the probability density function (PDF) at point x 
+    	along with the vector gradient of the PDF.
     """
